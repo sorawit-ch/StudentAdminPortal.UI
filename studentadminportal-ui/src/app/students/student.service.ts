e2cf43a186a12e7993f2c8d380fd3db6efe719df
sorawit2560 <sorawit72@hotmail.com>
2022-08-25 23:03:38 +0700

    edit student

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Student } from '../models/api-models/student.model';
+import { updatestudentRequest } from '../models/api-models/update-student-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,19 @@ export class StudentService {
 
   getStudent(studentId : string): Observable<Student>{
     return this.httpClient.get<Student>(this.baseApiUrl+'/students/'+ studentId)
+  }
+
+  updatestudent(studentId:string, studentRequest: Student): Observable<Student>{
+    const updateStudentRequest: updatestudentRequest={
+      firstName:studentRequest.firstName,
+      lastName:studentRequest.lastName,
+      dateOfBirth:studentRequest.dateOfBirth,
+      email:studentRequest.email,
+      mobile:studentRequest.mobile,
+      genderId:studentRequest.genderId,
+      physicalAddress:studentRequest.address.physicalAddress,
+      postalAddress:studentRequest.address.postalAddress
+    }
+    return this.httpClient.put<Student>(this.baseApiUrl+'/students/'+studentId, updateStudentRequest)
   }
 }
--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/services/gender.service';
 import { StudentService } from '../student.service';
 
 @Component({
@@ -30,9 +33,12 @@ export class ViewStudentComponent implements OnInit {
       postalAddress:''
     }
   }
+  genderList:Gender[]=[];
 
   constructor(private readonly studentService: StudentService,
-    private readonly route: ActivatedRoute) { }
+    private readonly route: ActivatedRoute,
+    private readonly genderService:GenderService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -43,15 +49,31 @@ export class ViewStudentComponent implements OnInit {
           this.studentService.getStudent(this.studentId).subscribe(
             (successResponse) => {
               this.student = successResponse;
-            },
-            (errorResponse) => {
-              console.log(errorResponse);
             }
           );
+          this.genderService.getGenderList().subscribe(
+            (successResponse) =>{
+              this.genderList = successResponse;
+            }
+          )
         }
+
       }
     );
 
+  }
+
+  onUpdate():void{
+    this.studentService.updatestudent(this.student.id, this.student).subscribe(
+      (successResponse)=>{
+        this.snackBar.open('Student update successfully', undefined,{
+          duration:2000
+        });
+      },
+      (errorResponse)=>{
+
+      }
+    )
   }
 
 }
