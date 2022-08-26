a57d061fae68c894cf0cb42bd728b18c5e571b31
sorawit2560 <sorawit72@hotmail.com>
2022-08-26 08:35:44 +0700

    Image Student

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -36,6 +36,7 @@ export class ViewStudentComponent implements OnInit {
 
   isNewStudent = false;
   header ="";
+  displayProfileImageUrl = '';
 
   genderList:Gender[]=[];
 
@@ -55,6 +56,7 @@ export class ViewStudentComponent implements OnInit {
           if(this.studentId.toLowerCase() === 'Add'.toLocaleLowerCase()){
             this.isNewStudent = true;
             this.header = 'Add New Student';
+            this.setImage();
           }else{
             this.isNewStudent = false;
             this.header = 'Edit Student';
@@ -62,6 +64,10 @@ export class ViewStudentComponent implements OnInit {
             this.studentService.getStudent(this.studentId).subscribe(
               (successResponse) => {
                 this.student = successResponse;
+                this.setImage();
+              },
+              (errorResponse)=>{
+                this.setImage();
               }
             );
           }
@@ -127,6 +133,34 @@ export class ViewStudentComponent implements OnInit {
 
       }
     )
+  }
+
+
+  uploadImage(event:any):void{
+    if(this.studentId){
+      const file: File = event.target.files[0];
+      this.studentService.uploadImage(this.student.id, file).subscribe(
+        (successResponse)=>{
+          this.student.profileImageUrl = successResponse;
+          this.setImage();
+
+          this.snackBar.open('Profile Image Updated', undefined,{
+            duration:2000
+          });
+        },
+        (errorResponse)=>{
+
+        }
+      )
+    }
+  }
+
+  private setImage():void{
+    if(this.student.profileImageUrl){
+      this.displayProfileImageUrl = this.studentService.getImagePath(this.student.profileImageUrl);
+    }else{
+      this.displayProfileImageUrl = '/assets/user.png';
+    }
   }
 
 }
